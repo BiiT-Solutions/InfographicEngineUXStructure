5faa7870a0a474ac6d45a3ed3aad8ba128302432
kbenlloch <kbenlloch@biit-solutions.com>
2023-12-13 14:06:40 +0100

    Updated library exports

--- projects/infographic-engine-lib/src/public-api.ts
+++ projects/infographic-engine-lib/src/public-api.ts
@@ -1,5 +1,22 @@
+export * from './lib/models/background'
+export * from './lib/models/border'
+export * from './lib/models/common-attribute'
+export * from './lib/models/element'
+export * from './lib/models/elementType'
+export * from './lib/models/generated-infographic'
+export * from './lib/models/gradient'
+export * from './lib/models/layout-type'
+export * from './lib/models/size-unit'
+export * from './lib/models/stop'
+export * from './lib/models/stroke-line'
+export * from './lib/models/vertical-align'
 
-export * from './lib/services/session.service';
 export * from './lib/services/auth.service';
+export * from './lib/services/caching.service';
+export * from './lib/services/infographic.service';
 export * from './lib/services/infographic-engine-root.service';
+export * from './lib/services/jpeg.service';
+export * from './lib/services/png.service';
+export * from './lib/services/session.service';
+export * from './lib/services/svg.service';
 
