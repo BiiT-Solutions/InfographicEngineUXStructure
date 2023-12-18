32d78108521346931cbdf2d0926b960c376a798d
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-12-18 12:59:14 +0100

    HttpParams is intended to be immutable

--- projects/infographic-engine-lib/src/lib/services/infographic.service.ts
+++ projects/infographic-engine-lib/src/lib/services/infographic.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {InfographicEngineRootService} from "./infographic-engine-root.service";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {GeneratedInfographic} from "../models/generated-infographic";
@@ -10,17 +10,22 @@ import {Observable} from "rxjs";
 export class InfographicService {
 
   private static readonly ROOT_PATH: string = '/infographic'
-  constructor(private rootService: InfographicEngineRootService, private httpClient: HttpClient) { }
+
+  constructor(private rootService: InfographicEngineRootService, private httpClient: HttpClient) {
+  }
 
   getAll(): Observable<GeneratedInfographic[]> {
     return this.httpClient.get<GeneratedInfographic[]>(`${this.rootService.serverUrl}${InfographicService.ROOT_PATH}`);
   }
+
   getById(id: number): Observable<GeneratedInfographic> {
     return this.httpClient.get<GeneratedInfographic>(`${this.rootService.serverUrl}${InfographicService.ROOT_PATH}/${id}`);
   }
+
   count(): Observable<number> {
     return this.httpClient.get<number>(`${this.rootService.serverUrl}${InfographicService.ROOT_PATH}/count`);
   }
+
   find(
     form?: string,
     version?: number,
@@ -29,63 +34,69 @@ export class InfographicService {
     from?: Date,
     to?: Date
   ): Observable<GeneratedInfographic[]> {
-    const httpParams: HttpParams = new HttpParams();
+    let httpParams: HttpParams = new HttpParams();
     if (form) {
-      httpParams.append('form', form);
+      httpParams = httpParams.append('form', form);
     }
     if (version) {
-      httpParams.append('version', version.toString());
+      httpParams = httpParams.append('version', version.toString());
     }
     if (createdBy) {
-      httpParams.append('createdBy', createdBy);
+      httpParams = httpParams.append('createdBy', createdBy);
     }
     if (organization) {
-      httpParams.append('organization', organization.toString());
+      httpParams = httpParams.append('organization', organization.toString());
     }
     if (from) {
-      httpParams.append('from', from.toISOString());
+      httpParams = httpParams.append('from', from.toISOString());
     }
     if (to) {
-      httpParams.append('to', to.toISOString());
+      httpParams = httpParams.append('to', to.toISOString());
     }
     return this.httpClient.get<GeneratedInfographic[]>(`${this.rootService.serverUrl}${InfographicService.ROOT_PATH}/find`, {params: httpParams});
   }
+
   findLatest(
     form?: string,
     version?: number,
     createdBy?: string,
     organization?: number,
   ): Observable<GeneratedInfographic> {
-    const httpParams: HttpParams = new HttpParams();
+    let httpParams: HttpParams = new HttpParams();
     if (form) {
-      httpParams.append('form', form);
+      httpParams = httpParams.append('form', form);
     }
     if (version) {
-      httpParams.append('version', version.toString());
+      httpParams = httpParams.append('version', version.toString());
     }
     if (createdBy) {
-      httpParams.append('createdBy', createdBy);
+      httpParams = httpParams.append('createdBy', createdBy);
     }
     if (organization) {
-      httpParams.append('organization', organization.toString());
+      httpParams = httpParams.append('organization', organization.toString());
     }
     return this.httpClient.get<GeneratedInfographic>(`${this.rootService.serverUrl}${InfographicService.ROOT_PATH}/find/latest`, {params: httpParams});
   }
+
   range(from: Date, to: Date): Observable<GeneratedInfographic[]> {
     const httpParams: HttpParams = new HttpParams();
     httpParams.append('from', from.toISOString());
     httpParams.append('to', to.toISOString());
     return this.httpClient.get<GeneratedInfographic[]>(`${this.rootService.serverUrl}${InfographicService.ROOT_PATH}/range`, {params: httpParams});
   }
+
   getAllCreated(): Observable<GeneratedInfographic[]> {
     return this.httpClient.get<GeneratedInfographic[]>(`${this.rootService.serverUrl}${InfographicService.ROOT_PATH}/users`);
   }
+
   countAllCreated(): Observable<number> {
     return this.httpClient.get<number>(`${this.rootService.serverUrl}${InfographicService.ROOT_PATH}/users/count`);
   }
+
   getAllCreatedByUser(username: string): Observable<GeneratedInfographic[]> {
     return this.httpClient.get<GeneratedInfographic[]>(`${this.rootService.serverUrl}${InfographicService.ROOT_PATH}/users/${username}`);
   }
+
   countAllCreatedByUser(username: string): Observable<number> {
     return this.httpClient.get<number>(`${this.rootService.serverUrl}${InfographicService.ROOT_PATH}/users/${username}/count`);
   }
